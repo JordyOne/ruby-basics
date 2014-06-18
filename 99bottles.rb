4404985efc5ebe05b8ff8c670f4b1031cd2a96ce
Jordan Dale <Jordan.T.Dale@gmail.com>
2014-06-17 21:16:22 -0600

    This is the refined version

--- 99bottles.rb
+++ 99bottles.rb
@@ -1,12 +1,38 @@
-puts "How manly are you?"
-bottles = gets.chomp.to_i
+answer = nil
+bottles = nil
+
 
 def counting_bottles (bottles)
   puts "#{bottles} of beer on the wall, #{bottles} of beer.  Take one down pass it around, #{bottles.to_i - 1} bottles of beer."
   bottles = bottles.to_i - 1
   if bottles != 0
-  counting_bottles (bottles)
+    sleep(0.5)
+    counting_bottles (bottles)
   end
 end
 
-counting_bottles (bottles)
+def be_specific (answer)
+  puts "I want to sing you a song, where should I start?"
+  answer = gets.chomp
+  bottles = answer.to_i
+  while true
+    if !answer.empty?
+      answer_arr = answer.split(/ /)
+      if answer_arr[0].downcase == 'sing'
+        if answer_arr[1].to_i > 0 && answer_arr[1].to_i < 100
+          bottles = answer_arr[1].to_i
+          counting_bottles (bottles)
+          break
+        else
+          puts "The number has to be greater than 0 and less than 100."
+          be_specific (answer)
+        end
+      else
+        puts "I must have forgotten to tell you, you gotta say \"sing\"..."
+        be_specific (answer)
+      end
+    end
+  end
+end
+
+be_specific (answer)
