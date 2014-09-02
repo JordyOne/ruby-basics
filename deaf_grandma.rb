6d755f3f74e6c66050a378ec8afa995ab62e3782
Jordan Dale <Jordan.T.Dale@gmail.com>
2014-09-02 15:12:53 -0600

    added a goodbye to grandma

--- deaf_grandma.rb
+++ deaf_grandma.rb
@@ -1,10 +1,17 @@
-
+puts "Tell me something grandson..."
+counter = 0
 
 while true
-  puts "Tell me something grandson..."
   print '> '
   something = gets.chomp
-  if something == something.upcase
+
+  if something == "GOODBYE"
+    counter += 1
+    if counter >= 3
+      puts "goodbye, thanks for stopping bye."
+      break
+    end
+  elsif something == something.upcase
     puts "NO, NOT SINCE " + (1930 + rand(20)).to_s + "!!!"
     break
   else
